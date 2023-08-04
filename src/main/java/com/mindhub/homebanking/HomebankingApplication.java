1273bf9e87d9dc8cc2f548ef89073eb70a5d323f
unknown <juanvillan777@gmail.com>
2023-08-04 01:30:56 -0300

    Client(Repo & Entity)

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,13 +1,25 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.Models.Client;
+import com.mindhub.homebanking.Repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository ClientR){
+		return (args) -> {
+			Client client = new Client("Melba", "Morel", "melba@mindhub.com", "00.000.000");
+			ClientR.save(client);
+		};
 	}
 
 }
---
+++ src/main/java/com/mindhub/homebanking/Models/Client.java
@@ -0,0 +1,62 @@
+package com.mindhub.homebanking.Models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String dni;
+
+    public Client (String firsName, String lastName, String email,String dni){
+        firsName = this.firstName;
+        lastName = this.lastName;
+        email = this.email;
+        dni = this.dni;
+    };
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
